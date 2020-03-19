1539f0a310a4727dd84689741881fabe6b7f3655
checksonhk <jackson.to@gmail.com>
2020-03-19 17:36:10 -0400

    feature: create new post

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView
+)
 
 # function based view
 def home(request):
@@ -25,3 +29,11 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
     # looking for <app>/<model>_<view_type>.html
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self , form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
