7fcb99feac2ad5e992583e1981cf47e7470cb74e
checksonhk <jackson.to@gmail.com>
2020-03-19 23:44:58 -0400

    feature: reset password

--- Bloggr/settings.py
+++ Bloggr/settings.py
@@ -131,3 +131,18 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
+from pathlib import Path
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path
+)
+EMAIL_HOST_USER = os.getenv('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_PASS')
+
