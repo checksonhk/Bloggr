433cb5771d330a2372e33b7bde490096dd6068d7
checksonhk <jackson.to@gmail.com>
2020-03-19 21:38:50 -0400

    feature: delete post

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView, 
     DetailView, 
-    CreateView
+    CreateView,
+    UpdateView,
+    DeleteView
 )
+
 
 # function based view
 def home(request):
@@ -30,10 +34,36 @@ class PostDetailView(DetailView):
     model = Post
     # looking for <app>/<model>_<view_type>.html
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title','content']
 
     def form_valid(self , form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+
+    model = Post
+    fields = ['title','content']
+
+    def form_valid(self , form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else: return False 
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+
+    model = Post
+    success_url = '/'    
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        else: return False 
